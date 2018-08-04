d8238feb74b814fec1ceea66df21607d40ea7c16
Arata Jingu <>
2018-08-04 17:42:22 +0900

    Initial Commit

---
+++ Assets/Scripts/SubstringImplement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubstringImplement : MonoBehaviour
+{
+    [SerializeField] Text speechText;
+    [SerializeField] float wordUpdateInterval = 0.1f;
+
+    public void Initialize(string word)
+    {
+        Observable.FromCoroutine<string>(observer => SubstringCoroutine(observer, word))
+                  .Subscribe(text => { speechText.text = text; })
+                  .AddTo(this);
+    }
+
+    IEnumerator SubstringCoroutine(IObserver<string> observer, string word)
+    {
+        for (int i = 1; i < word.Length+1; i++)
+        {
+            observer.OnNext(word.Substring(0, i));
+            yield return new WaitForSeconds(wordUpdateInterval);
+        }
+
+        observer.OnCompleted();
+    }
+}
---
+++ Assets/Scripts/WordPresenter.cs
@@ -0,0 +1,17 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordPresenter : MonoBehaviour 
+{
+    [SerializeField] Text inputText;
+    [SerializeField] Button submitButton;
+    [SerializeField] SubstringImplement substringImplement;
+
+	void Start()
+    {
+        submitButton.OnClickAsObservable()
+                    .Subscribe(_ => substringImplement.Initialize(inputText.text))
+                    .AddTo(this);
+    }
+}
